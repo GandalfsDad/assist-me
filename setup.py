54e100d91cfdeb8a9b84073c6fb37dec4dffc34d
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-07-03 12:51:34 +1000

    Uprev to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="AssistMe",
-    version="0.0.1",
+    version="0.0.2",
     author="GandalfsDad",
     description="CLI GPT helper",
     long_description=long_description,
