53742bb23d8ac6c407a31284d4f316db969e1f6b
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-07-03 13:29:06 +1000

    Simplify command options

--- AssistMe/CLI/main.py
+++ AssistMe/CLI/main.py
@@ -1,28 +1,27 @@
 import click
 from ..Engine import make_call
+from ..CLI import params as p
 
 @click.group()
-@click.option('--model','-m', default='GPT3', 
-              type=click.Choice(['GPT3', 'GPT4'], case_sensitive=False),
-              help='Model to use for generating text')
-@click.option('--name', '-n', default='Assistant')
 @click.pass_context
-def cli(ctx: click.core.Context, model: str, name: str) -> None:
+def cli(ctx: click.core.Context, **kwargs) -> None:
     ctx.ensure_object(dict)
-
-    ctx.obj['model'] = model
-    ctx.obj['name'] = name
 
 @cli.command('simple')
 @click.pass_context
-@click.option('--input', '-i',prompt = "You", help='Input text')
-def simple(ctx: click.core.Context, input: str) -> None:
-    ctx.obj['input'] = input
+@p._model
+@p._name
+@p._input
+def simple(ctx: click.core.Context, **kwargs) -> None:
+    ctx = p.parse_params(ctx, **kwargs)
     make_call(ctx)
 
 @cli.command('chat')
 @click.pass_context
-def chat(ctx: click.core.Context) -> None:
+@p._model
+@p._name
+def chat(ctx: click.core.Context, **kwargs) -> None:
+    ctx = p.parse_params(ctx, **kwargs)
     make_call(ctx)
 
 if __name__ == '__main__':
---
+++ AssistMe/CLI/params.py
@@ -0,0 +1,12 @@
+import click
+
+_name = click.option('--name', '-n', default='Assistant')
+_model = click.option('--model','-m', default='GPT3',
+                     type=click.Choice(['GPT3', 'GPT4'], case_sensitive=False),
+                    help='Model to use for generating text')
+_input = click.option('--input', '-i',prompt = "You", help='Input text')
+
+def parse_params(ctx: click.core.Context, **kwargs):
+    for key, value in kwargs.items():
+        ctx.obj[key] = value
+    return ctx
