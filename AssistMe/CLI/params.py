aafd9a863c3ff16c733bb7807d0052701e3e1e16
Rhian McClelland <rhianmcclelland193@hotmail.com>
2023-07-03 14:04:29 +1000

    Reorganise params

--- AssistMe/CLI/params.py
+++ AssistMe/CLI/params.py
@@ -1,10 +1,17 @@
 import click
 
-_name = click.option('--name', '-n', default='Assistant')
-_model = click.option('--model','-m', default='GPT3',
-                     type=click.Choice(['GPT3', 'GPT4'], case_sensitive=False),
-                    help='Model to use for generating text')
-_input = click.option('--input', '-i',prompt = "You", help='Input text')
+_name = click.option('--name', '-n', 
+                        default='Assistant', 
+                        help='Name of Assistant')
+
+_model = click.option('--model','-m', 
+                        default='GPT3',
+                        type=click.Choice(['GPT3', 'GPT4'], case_sensitive=False),
+                        help='Model to use for generating text')
+
+_input = click.option('--input', '-i',
+                        prompt = "You", 
+                        help='Input text')
 
 def parse_params(ctx: click.core.Context, **kwargs):
     for key, value in kwargs.items():
